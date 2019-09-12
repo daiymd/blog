4cf84da5a9d6ce37eec4d75cf5fb607916967ad7
dai <dai.poke@icloud.com>
2019-09-12 09:19:12 +0900

    インクリメンタルサーチ
    インクリメンタルサーチ時の、イメージがない記事の表示を変えた。

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,13 +2,20 @@ $(function(){
 
   function searchArticle(article) {
     article.text.length > 20 ? article_text = `${article.text.slice(0,20)}...` : article_text = article.text
+    article.title.length > 10 ? article_title = `${article.title.slice(0,10)}...` : article_title = article.title
+    if (!article.image) {
+      article_image = "http://design-ec.com/d/e_others_50/l_e_others_501.png"
+    }
+    else {
+      article_image = article.image
+    }
     var html = `<div class="left-article">
                   <div class="article">
                     <div class="article__image">
-                      <img src="${article.image}" width="220" height="200">
+                      <img src="${article_image}" width="220" height="200">
                     </div>
                     <div class="article__title">
-                      <a class="article__title_link" href="/articles/${article.id}">${article.title}</a>
+                      <a class="article__title_link" href="/articles/${article.id}">${article_title}</a>
                     </div>
                     <div class="article__latest-text">${article_text}</div>
                     <div class="article__user-name">
